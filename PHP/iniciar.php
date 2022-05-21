f4a57e4a2bbc000e202d4712192fc392e9d8fc91
sosaneitor <sss_sosa@hotmail.com>
2022-05-20 23:20:33 -0500

    segunda actualizacion de la pagina

--- PHP/iniciar.php
+++ PHP/iniciar.php
@@ -7,16 +7,21 @@ $clave=$_POST["clave"];
 
 $conectar = conn(); 
 $sql = "SELECT * FROM cliente WHERE correo='".$correo."' and clave ='".$clave."'";
+$nombre = "SELECT nombre from cliente WHERE correo='".$correo."'" ;
 $resul = mysqli_query($conectar, $sql) or
 trigger_error("Query Failed! SQL-Error: ".mysqli_error($conectar), E_USER_ERROR);
-
-
 $nr= mysqli_num_rows($resul);
+$resultado=mysqli_query($conectar, $nombre);
 
 if($nr == 1) {
+  $rows=mysqli_fetch_array($resultado);
+  $resultadoo=$rows[0];
+  session_start();
+  
+  $_SESSION["Nombre"]= $resultadoo;
+  echo "la variable de sesion tiene guardado " .$_SESSION["Nombre"];
 
-  echo"inicio con exito" ; 
-    
+  
 
 }
 else if ($nr == 0){
